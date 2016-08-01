1f3ec5f03f5f9753f69c411188f0288f25918058
Bean He <withhebing@gmail.com>
2016-08-01 18:37:48 +0800

    Initial commit

---
+++ ImageFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageFilter
+//
+//  Created by Beans on 16/8/1.
+//  Copyright © 2016年 iceWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageFilter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageFilter
+//
+//  Created by Beans on 16/8/1.
+//  Copyright © 2016年 iceWorks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
