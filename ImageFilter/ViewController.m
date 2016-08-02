63cfcaa71fe0a844d33c162c571809438abab507
Bean He <withhebing@gmail.com>
2016-08-02 12:15:23 +0800

    Correct bug and finish the rest

--- ImageFilter/ViewController.m
+++ ImageFilter/ViewController.m
@@ -31,7 +31,7 @@
     self.imgView.layer.shadowColor = [UIColor blackColor].CGColor;
     self.imgView.layer.shadowOffset = CGSizeMake(1, 1);
 
-    [self showFilters];
+//    [self showFilters];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,64 +43,93 @@
     NSArray *filterNames = [CIFilter filterNamesInCategory:kCICategoryColorEffect];
     for (NSString *filterName in filterNames) {
         NSLog(@"%@", filterName);
-//        CIFilter *filter = [CIFilter filterWithName:filterName];
-//        NSDictionary *attributes = filter.attributes;
-//        NSLog(@"%@", attributes);
+        CIFilter *filter = [CIFilter filterWithName:filterName];
+        NSDictionary *attributes = filter.attributes;
+        NSLog(@"%@", attributes);
     }
 }
 
 #pragma mark - photo effect
 
+- (void)outputImageWithFilterName:(NSString *)filterName {
+    //将UIImage转换成CIImage
+//    CIImage *ciImage = self.filterlessImage.CIImage; // 无效, 不显示; 为什么要新建一个呢
+    CIImage *ciImage = [[CIImage alloc] initWithImage:self.filterlessImage];
+    //创建滤镜
+    self.filter = [CIFilter filterWithName:filterName keysAndValues:kCIInputImageKey, ciImage, nil];
+    //已有的值不改变，其他的设为默认值
+    [self.filter setDefaults];
+
+    //渲染并输出CIImage
+    CIImage *outputImage = [self.filter outputImage];
+    //获取绘制上下文
+    self.context = [CIContext contextWithOptions:nil];
+
+
+    //创建CGImage句柄
+    CGImageRef cgImage = [self.context createCGImage:outputImage fromRect:[outputImage extent]];
+    //获取图片
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    //释放CGImage句柄
+    CGImageRelease(cgImage);
+    
+    self.imgView.image = image;
+}
+
 - (IBAction)photoEffectChrome {
     self.filter = [CIFilter filterWithName:@"CIPhotoEffectChrome"];
 
     // output
-    CIImage *inputImage = self.filterlessImage.CIImage;
+//    CIImage *inputImage = self.filterlessImage.CIImage;
+    CIImage *inputImage = [[CIImage alloc] initWithImage:self.filterlessImage];
     [self.filter setValue:inputImage forKey:kCIInputImageKey];
     CIImage *outputImage = self.filter.outputImage;
-//    self.imgView.image = [UIImage imageWithCIImage:outputImage];
+
+    self.context = [CIContext contextWithOptions:nil];
     CGImageRef cgImage = [self.context createCGImage:outputImage fromRect:outputImage.extent];
     self.imgView.image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);   // cgImage后释放
 }
 
 - (IBAction)photoEffectFade {
     self.filter = [CIFilter filterWithName:@"CIPhotoEffectFade"];
 
     // output
-    CIImage *inputImage = self.filterlessImage.CIImage;
+//    CIImage *inputImage = self.filterlessImage.CIImage;
+    CIImage *inputImage = [[CIImage alloc] initWithImage:self.filterlessImage];
     [self.filter setValue:inputImage forKey:kCIInputImageKey];
     CIImage *outputImage = self.filter.outputImage;
+
     self.imgView.image = [UIImage imageWithCIImage:outputImage];
-//    CGImageRef cgImage = [self.context createCGImage:outputImage fromRect:outputImage.extent];
-//    self.imgView.image = [UIImage imageWithCGImage:cgImage];
+    self.imgView.contentMode = UIViewContentModeScaleAspectFit;     // 大小有变化, 且调整不了
 }
 
 - (IBAction)photoEffectInstant {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectInstant"];
 }
 
 - (IBAction)photoEffectMono {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectMono"];
 }
 
 - (IBAction)photoEffectNoir {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectNoir"];
 }
 
 - (IBAction)photoEffectProgress {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectProcess"];
 }
 
 - (IBAction)photoEffectTonal {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectTonal"];
 }
 
 - (IBAction)photoEffectTransfer {
-
+    [self outputImageWithFilterName:@"CIPhotoEffectTransfer"];
 }
 
 - (IBAction)photoEffectOriginal {
-
+    self.imgView.image = self.filterlessImage;
 }
 
 - (IBAction)photoEffectAutoAdjust {
